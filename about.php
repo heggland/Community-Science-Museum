55ebc9b8c2f0010d87019d0ec6936d98afed2773
Kjetil <kjetilheggland@gmail.com>
2020-05-03 23:31:28 +0200

    html build
    html build

---
+++ about.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>About us</title>
+</head>
+<?php include_once 'includes\nav.html'; ?>
+<?php include_once 'includes\footer.html'; ?>
+
+<body>
+<h2>Who are we?</h2>
+<p>Come join us at the Community Science Museum where we’re committed to making science accessible to all.</p>
+<p>Over the course of human history, science has developed from our early understanding of fire, wind, water, and earth
+    to exploring everything from galaxies far away to the very building blocks of life itself.</p>
+<p>The aim of our museum is to create a space where everyone can experience the wonders of our discoveries and perhaps
+    even ignite a lifelong passion to continue the exploration of the world around us.</p>
+<p>We believe science should not be confined to the textbook, but brought to live through exhibits. This is why we have
+    over 1000 different exhibits on the many varied subjects of science to explore. Many of these exhibits are designed
+    for you to interact with and play around to see science come to life (apart from the dinosaur exhibits – they only
+    come to life at night when everyone’s gone home).</p>
+
+<form></form>
+
+</body>
+</html>
---
+++ events.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Events</title>
+</head>
+<?php include_once 'includes\nav.html'; ?>
+
+
+<body>
+
+<h1>News</h1>
+<h2>Visiting Professor of Aeronautics</h2>
+<p>It is our pleasure to announce that Prof Sheila Widnall from the Massachusetts Institute of Technology will be
+    delivering 3 lectures on the development of aeronautics and where the future lies in this exciting ‘space’.</p>
+
+<h2>Night in the Museum</h2>
+<p>Get your family together for an exciting night in the museum as you sleep over beside dinosaurs and science displays.
+    Bring your own sleeping bag and get ready to rough it as we go exploring the wonders of science.</p>
+
+<h2>Energetica Exhibition on Loan</h2>
+<p>On loan from the NEMO Science Museum in Amsterdam, the Energetica exhibition is coming to the Community Science
+    Museum. It’s a series of installations that allow visitors to experience the power of the elements as we harness
+    them. From solar energy powering lighting, to ‘Wind Island’ that shows how turbines are able to use and control wind
+    to create power.</p>
+
+</body>
+<?php include_once 'includes\footer.html'; ?>
+</html>
